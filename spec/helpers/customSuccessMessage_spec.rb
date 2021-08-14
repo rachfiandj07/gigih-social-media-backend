4de85ffae92aae8239ce05d63a0158a2d77dae68
Muhammad Naufal Rachfian dJ <naufalrdj@gmail.com>
2021-08-15 00:32:44 +0700

    [TEST][RED] shoudl raise sucess custom message test

---
+++ spec/helpers/customSuccessMessage_spec.rb
@@ -0,0 +1,9 @@
+
+describe CustomSuccessMessage do
+    it 'should raise sucess custom message ' do
+        customSucessMessage = CustomSuccessMessage.new('Success', 201)
+
+        expect(customSucessMessage.message).to eq('Success')
+        expect(customSucessMessage.status).to eq(201)
+    end
+end
