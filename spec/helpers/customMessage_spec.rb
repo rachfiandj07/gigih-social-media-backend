064054c7852748192c3734a7c647bbc1fecc7961
Muhammad Naufal Rachfian dJ <naufalrdj@gmail.com>
2021-08-14 23:33:39 +0700

    Change description name on customMessage_spec

--- spec/helpers/customMessage_spec.rb
+++ spec/helpers/customMessage_spec.rb
@@ -1,13 +1,13 @@
 require_relative '../../app/helpers/customErrorMessage.rb'
 
 describe CustomErrorMessage do
-    it 'raise custom message without status code' do
+    it 'should raise custom message without status code' do
         customErrorMessage = CustomErrorMessage.new('failed to crud with external service')
 
         expect(customErrorMessage.message).to eq("failed to crud with external service")
     end
 
-    it 'raise custom message with status code' do
+    it 'should raise custom message with status code' do
         customErrorMessage = CustomErrorMessage.new('failed to crud with external service', 401)
 
         expect(customErrorMessage.message).to eq("failed to crud with external service")
