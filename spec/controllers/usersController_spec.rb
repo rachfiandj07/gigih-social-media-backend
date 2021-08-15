9d3912b51554b68330e306b4a717f9bcd0c7c5c9
Muhammad Naufal Rachfian dJ <naufalrdj@gmail.com>
2021-08-15 17:46:55 +0700

    [TEST][RED] register user should return response status 200 test-case

---
+++ spec/controllers/usersController_spec.rb
@@ -0,0 +1,42 @@
+require_relative '../../controllers/usersController.rb'
+
+describe UsersController do
+    before :each do
+        @stub_client = double()
+        @controller = UsersController.new()
+    end
+    
+    describe 'register user' do
+        context 'given valid params' do
+            it 'should return response status 200' do
+                allow(Users).to receive(:new).and_return(@stub_client)
+                expect(@stub_client).to receive(:register)
+
+                response = {
+                    "message" => "Success",
+                    "status" => 200,
+                    "data" => {
+                        "user_id" => 1,
+                        "username" => "naufalrdj",
+                        "bio" => "Hello",
+                        "email" => "test@gmail.com",
+                        "createdAt" => "2021-08-15 00:51:03",
+                        "updatedAt" => "2021-08-15 00:51:03"
+                    }
+                }
+
+                params = {
+                    "user_id" => 1,
+                    "username" => "naufalrdj",
+                    "bio" => "Hello",
+                    "email" => "test@gmail.com",
+                    "createdAt" => "2021-08-15 00:51:03",
+                    "updatedAt" => "2021-08-15 00:51:03"
+                }
+
+                result = @controller.register(params)
+                expect(result).to eq(response)
+            end
+        end
+    end
+end
