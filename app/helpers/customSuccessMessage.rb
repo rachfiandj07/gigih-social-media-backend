0102968668ef384c2148751a5aa5505ea752c017
Muhammad Naufal Rachfian dJ <naufalrdj@gmail.com>
2021-08-16 02:39:19 +0700

    [TEST][REFACTOR][GREEN] add method attr, and add test for CustomSuccessMessage

--- app/helpers/customSuccessMessage.rb
+++ app/helpers/customSuccessMessage.rb
@@ -1,8 +1,9 @@
 class CustomSuccessMessage
-    attr_accessor :message, :status
+    attr_accessor :message, :status, :method
 
-    def initialize(message='Success', status=200)
+    def initialize(message='Success', status=200, method)
         @message = message
         @status = status
+        @method = method
     end
 end
--- spec/helpers/customSuccessMessage_spec.rb
+++ spec/helpers/customSuccessMessage_spec.rb
@@ -2,9 +2,10 @@ require_relative '../../app/helpers/customSuccessMessage.rb'
  
 describe CustomSuccessMessage do
     it 'should raise sucess custom message ' do
-        customSucessMessage = CustomSuccessMessage.new('Success', 201)
+        customSucessMessage = CustomSuccessMessage.new('Success', 201, 'POST')
 
         expect(customSucessMessage.message).to eq('Success')
         expect(customSucessMessage.status).to eq(201)
+        expect(customSucessMessage.method).to eq('POST')
     end
 end
