1d4abbd723b8f6183e593dfb080f7c8897716a0b
Muhammad Naufal Rachfian dJ <naufalrdj@gmail.com>
2021-08-17 21:59:26 +0700

    [TEST][RED] get list trending hashtag should return response hashtag name with count test

--- spec/models/post_hashtags_spec.rb
+++ spec/models/post_hashtags_spec.rb
@@ -31,4 +31,28 @@ describe PostHashtags do
       @post_hashtags.get_post_contain_hashtag
     end
   end
+
+  describe 'get list trending hashtag' do
+    it 'should return response by hashtag name with count' do
+      response = {
+        'hashtag_id' => 1,
+        'count' => 4,
+        'name' => 'gigih'
+      }
+
+      stub_query = "SELECT hashtags.hashtag_id, COUNT(hashtags.hashtag_id) as count, hashtags.`name`
+          FROM user_posts
+          LEFT JOIN post_hashtags ON user_posts.post_id = post_hashtags.post_id
+          LEFT JOIN hashtags ON hashtags.hashtag_id = post_hashtags.hashtag_id
+          LEFT JOIN users ON users.user_id = user_posts.user_id
+          WHERE user_posts.createdAt > DATE_SUB(NOW(), INTERVAL 24 HOUR)
+          GROUP BY hashtags.hashtag_id
+          ORDER BY count DESC
+          LIMIT 5"
+
+      expect(@stub_client).to receive(:query).with(stub_query).and_return([response])
+
+      @post_hashtag.get_list_trending_hashtag
+    end
+  end
 end
