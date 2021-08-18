e6a120df396435d4c1e3a52dd1530bf86c7bda8d
Muhammad Naufal Rachfian dJ <naufalrdj@gmail.com>
2021-08-18 22:28:43 +0700

    [TEST][RED] get list trending hashtag should return 5 top hashtag test-case

--- spec/controllers/post_hashtagsController_spec.rb
+++ spec/controllers/post_hashtagsController_spec.rb
@@ -1,39 +1,61 @@
-require_relative '../../controllers/post_hashtagsController.rb'
+require_relative '../../controllers/post_hashtagsController'
 
 describe PostHashtagsController do
-    before :each do
-        @stub_client = double()
-        @controller = PostHashtagsController.new
+  before :each do
+    @stub_client = double
+    @controller = PostHashtagsController.new
+  end
+
+  describe 'search post hashtag' do
+    context 'with available hashtag' do
+      it 'should return search response post contain hastag' do
+        params = {
+          'name' => 'gigih'
+        }
+
+        response = {
+          'message' => 'Success',
+          'status' => 200,
+          'method' => 'POST',
+          'data' => [{
+            'post_id' => 1,
+            'username' => 'naufalrdj',
+            'description' => 'Hello #gigih',
+            'attachment' => nil,
+            'parent_id' => nil,
+            'createdAt' => '2021-08-15 00:51:03',
+            'updatedAt' => '2021-08-15 00:51:03'
+          }]
+        }
+
+        allow(@stub_client).to receive(:get_post_contain_hashtag).and_return(response['data'])
+        allow(PostHashtags).to receive(:new).and_return(@stub_client)
+
+        result = @controller.search_post_hashtag(params)
+        expect(result).to eq(response)
+      end
     end
+  end
+  describe 'get list trending hashtag' do
+    context 'top 5 trending past 24 hours' do
+      it 'should return top 5 hashtag' do
+        response = {
+          'message' => 'Success',
+          'status' => 200,
+          'method' => 'POST',
+          'data' => [{
+            'hashtag_id' => 1,
+            'count' => 10,
+            'description' => 'gigih'
+          }]
+        }
 
-    describe 'search post hashtag' do
-        context 'with available hashtag' do
-            it 'should return search response post contain hastag' do
-                params = {
-                    "name" => "gigih"
-                }
+        allow(@stub_client).to receive(:get_list_trending_hashtag).and_return(response['data'])
+        allow(PostHashtags).to receive(:new).and_return(@stub_client)
 
-                response = {
-                    'message' => 'Success',
-                    'status' => 200,
-                    'method' => 'POST',
-                    'data' => [{
-                      'post_id' => 1,
-                      'username' => 'naufalrdj',
-                      'description' => 'Hello #gigih',
-                      'attachment' => nil,
-                      'parent_id' => nil,
-                      'createdAt' => '2021-08-15 00:51:03',
-                      'updatedAt' => '2021-08-15 00:51:03'
-                    }]
-                }
-
-                allow(@stub_client).to receive(:get_post_contain_hashtag).and_return(response['data'])
-                allow(PostHashtags).to receive(:new).and_return(@stub_client)
-
-                result = @controller.search_post_hashtag(params)
-                expect(result).to eq(response)
-            end
-        end
+        result = @controller.get_list_trending
+        expect(result).to eq(response)
+      end
     end
+  end
 end
